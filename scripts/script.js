ff30a3ac41bea3207c17d0dfc02409fef905a73c
proekt-rest <alesja92@mail.ru>
2012-04-19 18:01:09 +0400

    Update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,25 +8,30 @@ $("#content").load("./sources/main-site.html");
 	   $("#main-site").addClass("current") ;  
 	   });
 	   
-	  $("#bron_img").click(function(){
+	  $("#bron_id").click(function(){
 	   $('li').removeClass("current");
 	 $("#content").load("./sources/bron.html") ; 
- $("#bron_img").addClass("current") ;  	 
+ $("#bron_id").addClass("current") ;  	 
 	   });
 	   
-	    $("#zakaz_img").click(function(){
+	    $("#zakaz_id").click(function(){
 		 $('li').removeClass("current");
 	   $("#content").load("./sources/zakaz.html");  
- $("#zakaz_img").addClass("current") ;	   
+ $("#zakaz_id").addClass("current") ;	   
 	   });
-	    $("#your_img").click(function(){
+	    $("#your_id").click(function(){
 		 $('li').removeClass("current");
 	   $("#content").load("./sources/your.html"); 
-	    $("#your_img").addClass("current") ;
+	    $("#your_id").addClass("current") ;
 	   });
-	    $("#galer_img").click(function(){
+	    $("#menu_id").click(function(){
+		 $('li').removeClass("current");
+	   $("#content").load("./sources/menu.html");  
+	    $("#menu_id").addClass("current") ;
+	   });
+	    $("#galer_id").click(function(){
 		 $('li').removeClass("current");
 	   $("#content").load("./sources/galer.html");  
-	    $("#galer_img").addClass("current") ;
+	    $("#galer_id").addClass("current") ;
 	   });
 	});
