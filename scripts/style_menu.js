db017ea48e72d4b059cc13d61c6ce2f46bbc4937
proekt-rest <alesja92@mail.ru>
2012-04-26 23:29:34 +0400

    Проект обновлен

--- scripts/style_menu.js
+++ scripts/style_menu.js
@@ -1,19 +1,17 @@
 $(document).ready(function(){
-$("#list").load("./sources/sushi.html");
- $("#sushi").addClass("current_menu") ;
 	  $("#sushi").click(function(){
 	   $('li').removeClass("current_menu");
-	   $("#list").load("./sources/sushi.html");
+	   $("#all_menu").load("sources/sushi.html");
 	   $("#sushi").addClass("current_menu") ;  
 	   });
 	   $("#anti").click(function(){
 	   $('li').removeClass("current_menu");
-	   $("#list").load("./sources/anti.html");
+	   $("#all_menu").load("sources/antisushi.html");
 	   $("#anti").addClass("current_menu") ;  
 	   });
 	   $("#napitki").click(function(){
 	   $('li').removeClass("current_menu");
-	   $("#list").load("./sources/napitki.html");
+	   $("#all_menu").load("sources/napitki.html");
 	   $("#napitki").addClass("current_menu") ;  
 	   });
 	   
